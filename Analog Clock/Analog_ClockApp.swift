6603ce42a847792f4f0da2f67262b814d56a84be
Kadir Ince <kadirincedev@gmail.com>
2020-09-13 14:19:10 +0300

    Initial Commit

---
+++ Analog Clock/Analog_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Analog_ClockApp.swift
+//  Analog Clock
+//
+//  Created by Kadir on 13.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct Analog_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

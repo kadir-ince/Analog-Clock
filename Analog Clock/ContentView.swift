b9a1c3d9e8c4d114e5e2a56323c78386e5e45aa6
Kadir Ince <kadirincedev@gmail.com>
2020-09-13 15:21:45 +0300

    Remove Locale Name

--- Analog Clock/ContentView.swift
+++ Analog Clock/ContentView.swift
@@ -96,12 +96,13 @@ struct Home: View {
             }
             .frame(width: screen.width - 80, height: screen.width - 80)
 
-            
-            // Getting Region Name
-            Text(Locale.current.localizedString(forRegionCode: Locale.current.regionCode!) ?? "")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .padding(.top, 35)
+//            
+//            // Getting Region Name
+//            let locale = Locale.current
+//            Text(locale.regionCode ?? "")
+//                .font(.largeTitle)
+//                .fontWeight(.heavy)
+//                .padding(.top, 35)
             
             Text(getTime())
                 .font(.system(size: 45))

ce76cbe8851a57f07df578374c1d1173d0e5c935
Kadir Ince <kadirincedev@gmail.com>
2020-09-13 14:32:51 +0300

    Created Change Color Scheme

--- Analog Clock/ContentView.swift
+++ Analog Clock/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isDark = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Home(isDark: $isDark)
+                .navigationBarHidden(true)
+                .preferredColorScheme(isDark ? .dark : .light)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    @Binding var isDark: Bool
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Analog Clock")
+                    .font(.title)
+                    .fontWeight(.heavy)
+
+                Spacer()
+
+                Button(action: { isDark.toggle() }, label: {
+                    Image(systemName: isDark ? "sun.min.fill" : "moon.fill")
+                        .font(.system(size: 22))
+                        .foregroundColor(isDark ? .black : .white)
+                        .padding()
+                        .background(Color.primary)
+                        .clipShape(Circle())
+
+                })
+            }
+            .padding()
+
+            Spacer(minLength: 0)
+        }
     }
 }
